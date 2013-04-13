1e29dabd40f00af35d2098a0cfaee23422152977
Ivan Dmitrievsky <dmitrievskiy.ivan@gmail.com>
2013-04-13 20:51:41 +0400

    added combs module

---
+++ combs.c
@@ -0,0 +1,9 @@
+//
+//  combs.c
+//  Combinatorics
+//
+//  Created by ivan on 13.04.13.
+//  Copyright (c) 2013 dmitrievsky. All rights reserved.
+//
+
+#include "combs.h"
---
+++ combs.h
@@ -0,0 +1,14 @@
+//
+//  combs.h
+//  Combinatorics
+//
+//  Created by ivan on 13.04.13.
+//  Copyright (c) 2013 dmitrievsky. All rights reserved.
+//
+
+#ifndef Combinatorics_combs_c
+#define Combinatorics_combs_c
+
+
+
+#endif
